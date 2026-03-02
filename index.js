86821d9925eac8b7500d75647f5734d3eafb83e9
Alpha Tasker <81807892+thealphatasker@users.noreply.github.com>
2026-03-02 15:26:30 +0500

    u1.1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello Server!!")
+import express from "express";
+
+const app = express();
+
+app.get("/weather", (req, res) => {
+  res.send("Hey, Today is the sunny day.");
+});
+
+app.get("/alpha", (req, res) => {
+  res.send("Hey, this is in beta.");
+});
+
+app.listen(8000, () => {
+  console.log("Server is running!");
+});
